f3f7f97e96b06ec3af7989a739ee82b39ea81992
Jyoti <sainijyoti042002@gmail.com>
2023-05-26 07:40:04 +0530

    new change

--- src/components/Routers.js
+++ src/components/Routers.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
-// import More from './More';
 import MoreAbout from './More';
+
 const Routers = () => {
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/more" element={<MoreAbout />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/more" element={<MoreAbout />} />
+    </Routes>
+    <Footer />
+  </Router>
   );
 };
 
